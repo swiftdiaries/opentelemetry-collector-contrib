8f8355908d944539e37e2de3c2ed7d1f0df690ab
Adhita Selvaraj <adhita.selvaraj@gmail.com>
2024-05-05 19:57:43 +0000

    add exporter for GCS

---
+++ exporter/googlecloudexporter/exporter.go
@@ -0,0 +1,19 @@
+package googlecloudexporter
+
+import (
+	"go.opentelemetry.io/collector/exporter"
+	"go.uber.org/zap"
+)
+
+type gcsExporter struct {
+	config *Config
+	logger *zap.Logger
+	//TODO(swiftdiaries): Create datawriter, marshaler similar to S3Exporter
+}
+
+func newGCSExporter(config *Config, params exporter.CreateSettings) *gcsExporter {
+	gcsExporter := &gcsExporter{
+		config: config,
+		logger: params.Logger,
+	}
+}
